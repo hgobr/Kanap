da37986d8e7c8ffd09e925fdd0ecd7940ed6c8c6
hugo <hugo.brothier@hotmail.fr>
2022-08-06 16:46:11 +0200

    update product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,34 @@
 const url = window.location.href;
 
 const productId = new URL(url).searchParams.get('id');
+
+fetch('http://localhost:3000/api/products/' + productId).then((res) =>
+  res.json().then((data) => {
+    // console.log(data.colors);
+
+    const img = document.createElement('img');
+    img.setAttribute('src', data.imageUrl);
+    img.setAttribute('alt', data.altTxt);
+    document.querySelector('.item__img').appendChild(img);
+
+    document.getElementById('title').innerHTML = data.name;
+
+    document.getElementById('price').innerHTML = data.price;
+
+    document.getElementById('description').innerHTML = data.description;
+
+    const selectColor = document.getElementById('colors');
+
+    const productColors = data.colors;
+
+    for (color of productColors) {
+      const option = document.createElement('option');
+
+      option.setAttribute('value', color);
+
+      option.innerHTML = color;
+
+      selectColor.appendChild(option);
+    }
+  })
+);
