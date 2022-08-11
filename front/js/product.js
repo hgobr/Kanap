32b22a24846d76b907c1c1c7e49d085dc78d6ce9
hugo <hugo.brothier@hotmail.fr>
2022-08-12 00:47:05 +0200

    html & js update

--- front/js/product.js
+++ front/js/product.js
@@ -2,22 +2,25 @@ const url = window.location.href;
 
 const productId = new URL(url).searchParams.get('id');
 
+const addProductButton = document.getElementById('addToCart');
+const itemsQuantity = document.getElementById('quantity');
+const selectColor = document.getElementById('colors');
+
+// selectColor.addEventListener('click', () => {
+//   console.log(selectColor.value);
+// });
+
 fetch('http://localhost:3000/api/products/' + productId).then((res) =>
   res.json().then((data) => {
-    // console.log(data.colors);
-
     const img = document.createElement('img');
+
     img.setAttribute('src', data.imageUrl);
     img.setAttribute('alt', data.altTxt);
+
     document.querySelector('.item__img').appendChild(img);
-
     document.getElementById('title').innerHTML = data.name;
-
     document.getElementById('price').innerHTML = data.price;
-
     document.getElementById('description').innerHTML = data.description;
-
-    const selectColor = document.getElementById('colors');
 
     const productColors = data.colors;
 
@@ -25,10 +28,18 @@ fetch('http://localhost:3000/api/products/' + productId).then((res) =>
       const option = document.createElement('option');
 
       option.setAttribute('value', color);
-
       option.innerHTML = color;
-
       selectColor.appendChild(option);
     }
+
+    const newItem = () => {
+      localStorage.setItem('id', data._id);
+      localStorage.setItem('quantity', itemsQuantity.value);
+      localStorage.setItem('color', selectColor.value);
+    };
+
+    addProductButton.addEventListener('click', () => {
+      newItem();
+    });
   })
 );
